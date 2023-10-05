d35b1058ed4f24d39200aabdbbce5bb4bfc0b68b
Edgar Xuc <xucxole@gmail.com>
2023-10-04 20:21:14 -0600

    first commit

--- MiMascota.sql
+++ MiMascota.sql
@@ -35,18 +35,18 @@ CREATE TABLE Tratamientos (
     id_tratamiento INT PRIMARY KEY,
     id_cita INT,
     descripcion TEXT,
-    costo DECIMAL(10, 2),
+    costo DECIMAL(10, 5),
     FOREIGN KEY (id_cita) REFERENCES Citas(id_cita)
 );
 
 
 INSERT INTO Personas (nit, nombre, direccion, telefono) VALUES
-(1, 'Juan Pérez', 'Calle 123', '555-1234'),
-(2, 'María López', 'Avenida 456', '555-5678');
+(1, 'Daniel Estrada', 'Calle 123', '5632-6934'),
+(2, 'María López', 'Avenida 456', '2356-5698');
 
 INSERT INTO Mascotas (id_mascota, nombre, fecha_nacimiento, fecha_registro, id_persona_responsable) VALUES
-(1, 'Firulais', '2018-05-10', '2022-01-15', 1),
-(2, 'Pelusa', '2019-08-20', '2022-02-03', 1);
+(1, 'Negrito', '2018-05-10', '2023-05-15', 1),
+(2, 'Palin', '2019-08-20', '2023-09-03', 1);
 
 
 INSERT INTO Citas (id_cita, fecha_hora, id_mascota, id_persona_responsable, descripcion, correlativo_cita, presentacion, fecha_siguiente_cita) VALUES
@@ -74,15 +74,15 @@ ORDER BY Mascotas.fecha_registro;
 
 SELECT Citas.*
 FROM Citas
-WHERE Citas.id_mascota = 'ID_DE_LA_MASCOTA';
+WHERE Citas.id_mascota = 'id_de_la_mascota';
 
 SELECT *
 FROM Citas
-WHERE EXTRACT(MONTH FROM fecha_hora) = 'MES_ESPECIFICO';
+WHERE EXTRACT(MONTH FROM fecha_hora) = 'mes_especifico';
 
 SELECT *
 FROM Citas
-WHERE fecha_hora >= 'FECHA_INICIO' AND fecha_hora < 'FECHA_FIN'
+WHERE fecha_hora >= 'fecha_inicio' AND fecha_hora < 'fecha_fin'
 ORDER BY fecha_hora;
 
 SELECT id_mascota, COUNT(*) AS total_citas
@@ -99,11 +99,11 @@ GROUP BY Personas.nit, Personas.nombre;
 SELECT Tratamientos.descripcion, Tratamientos.costo
 FROM Tratamientos
 INNER JOIN Citas ON Tratamientos.id_cita = Citas.id_cita
-WHERE Citas.id_mascota = 'ID_DE_LA_MASCOTA';
+WHERE Citas.id_mascota = 'id_de_la_mascota';
 
 SELECT Mascotas.nombre AS NombreMascota, SUM(Tratamientos.costo) AS CostoTotalTratamientos
 FROM Tratamientos
 INNER JOIN Citas ON Tratamientos.id_cita = Citas.id_cita
 INNER JOIN Mascotas ON Citas.id_mascota = Mascotas.id_mascota
-WHERE Mascotas.id_persona_responsable = 'NIT_DE_LA_PERSONA'
+WHERE Mascotas.id_persona_responsable = 'nit_de_la_persona'
 GROUP BY Mascotas.id_mascota, Mascotas.nombre;
